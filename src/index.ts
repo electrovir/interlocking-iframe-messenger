21e99d3e82bc55631e1726c2df0c7442bbb35f9c
electrovir <electrovir@users.noreply.github.com>
2023-07-11 23:29:31 +0000

    export GlobalMessenger type

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './errors/iframe-disconnected.error';
 export * from './messenger/create-messenger';
+export * from './messenger/global-object-for-messaging';
 export * from './messenger/iframe-messenger';
 export * from './messenger/messenger-inputs';
 export * from './set-debug-mode';
