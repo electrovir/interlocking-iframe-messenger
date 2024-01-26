e10be7f23bea397f292cdff80480dd7dffd29357
electrovir <electrovir@users.noreply.github.com>
2024-01-26 22:30:01 +0000

    [dev] fix demo build

--- configs/vite.config.ts
+++ configs/vite.config.ts
@@ -1,3 +1,14 @@
+import {resolve} from 'path';
 import {defineConfig} from 'virmator/dist/compiled-base-configs/base-vite';
 
-export default defineConfig({forGitHubPages: true}, {});
+export default defineConfig(
+    {
+        forGitHubPages: true,
+        packageDirPath: resolve(__dirname, '..'),
+    },
+    (baseConfig) => {
+        return {
+            ...baseConfig,
+        };
+    },
+);
